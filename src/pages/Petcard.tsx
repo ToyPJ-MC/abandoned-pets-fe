3aa93992bd3fb27321a1c575bdf90357c2c486e0
anten <chch4lee1@naver.com>
2023-01-03 10:34:07 +0900

    etri에서 업무줌..

--- src/pages/Petcard.tsx
+++ src/pages/Petcard.tsx
@@ -1,88 +1,89 @@
-import React from "react";
-import {
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Typography,
-  Button,
-  Stack,
-} from "@mui/material";
+import React, { useEffect } from 'react';
+import { Card, CardActions, CardContent, CardMedia, Typography, Button, Stack } from '@mui/material';
+import { allAPI } from '../api/server';
+import { useRecoilState } from 'recoil';
+import { petcardDataState } from '../states/atom';
+
+const [alldata, setAlldata] = useRecoilState(petcardDataState);
+
+useEffect(() => {
+   allAPI(setAlldata);
+}, [alldata]);
 
 const Petcard = () => {
-  return (
-    <>
-      <Stack direction="row">
-        <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
-          <CardMedia></CardMedia>
-          <CardContent>
-            <Typography variant="body1" color="text.secondary">
-              Test1
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Detail</Button>
-          </CardActions>
-        </Card>
-        <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
-          <CardMedia></CardMedia>
-          <CardContent>
-            <Typography variant="body1" color="text.secondary">
-              Test1
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Detail</Button>
-          </CardActions>
-        </Card>
-        <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
-          <CardMedia></CardMedia>
-          <CardContent>
-            <Typography variant="body1" color="text.secondary">
-              Test1
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Detail</Button>
-          </CardActions>
-        </Card>
-      </Stack>
-      <Stack direction="row" sx={{ marginTop: 10 }}>
-        <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
-          <CardMedia></CardMedia>
-          <CardContent>
-            <Typography variant="body1" color="text.secondary">
-              Test1
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Detail</Button>
-          </CardActions>
-        </Card>
-        <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
-          <CardMedia></CardMedia>
-          <CardContent>
-            <Typography variant="body1" color="text.secondary">
-              Test1
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Detail</Button>
-          </CardActions>
-        </Card>
-        <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
-          <CardMedia></CardMedia>
-          <CardContent>
-            <Typography variant="body1" color="text.secondary">
-              Test1
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Detail</Button>
-          </CardActions>
-        </Card>
-      </Stack>
-    </>
-  );
+   return (
+      <>
+         <Stack direction="row">
+            <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
+               <CardMedia></CardMedia>
+               <CardContent>
+                  <Typography variant="body1" color="text.secondary">
+                     Test1
+                  </Typography>
+               </CardContent>
+               <CardActions>
+                  <Button size="small">Detail</Button>
+               </CardActions>
+            </Card>
+            <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
+               <CardMedia></CardMedia>
+               <CardContent>
+                  <Typography variant="body1" color="text.secondary">
+                     Test1
+                  </Typography>
+               </CardContent>
+               <CardActions>
+                  <Button size="small">Detail</Button>
+               </CardActions>
+            </Card>
+            <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
+               <CardMedia></CardMedia>
+               <CardContent>
+                  <Typography variant="body1" color="text.secondary">
+                     Test1
+                  </Typography>
+               </CardContent>
+               <CardActions>
+                  <Button size="small">Detail</Button>
+               </CardActions>
+            </Card>
+         </Stack>
+         <Stack direction="row" sx={{ marginTop: 10 }}>
+            <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
+               <CardMedia></CardMedia>
+               <CardContent>
+                  <Typography variant="body1" color="text.secondary">
+                     Test1
+                  </Typography>
+               </CardContent>
+               <CardActions>
+                  <Button size="small">Detail</Button>
+               </CardActions>
+            </Card>
+            <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
+               <CardMedia></CardMedia>
+               <CardContent>
+                  <Typography variant="body1" color="text.secondary">
+                     Test1
+                  </Typography>
+               </CardContent>
+               <CardActions>
+                  <Button size="small">Detail</Button>
+               </CardActions>
+            </Card>
+            <Card sx={{ maxWidth: 300, marginLeft: 10 }}>
+               <CardMedia></CardMedia>
+               <CardContent>
+                  <Typography variant="body1" color="text.secondary">
+                     Test1
+                  </Typography>
+               </CardContent>
+               <CardActions>
+                  <Button size="small">Detail</Button>
+               </CardActions>
+            </Card>
+         </Stack>
+      </>
+   );
 };
 export default Petcard;
--- src/states/atom.tsx
+++ src/states/atom.tsx
@@ -1,95 +1,126 @@
-import { atom } from "recoil";
+import { atom } from 'recoil';
 
 const gunguDataState = atom({
-  key: "gun",
-  default: ["없음"],
+   key: 'gun',
+   default: ['없음'],
 });
 const placeDataState = atom({
-  key: "place",
-  default: ["없음"],
+   key: 'place',
+   default: ['없음'],
 });
 const indexDataState = atom({
-  key: "index",
-  default: ["default"],
+   key: 'index',
+   default: ['default'],
 });
 const animalDataState = atom({
-  key: "animal",
-  default: [
-    {
-      id: 417000,
-      name: "개",
-    },
-    {
-      id: 422400,
-      name: "고양이",
-    },
-  ],
+   key: 'animal',
+   default: [
+      {
+         id: 417000,
+         name: '개',
+      },
+      {
+         id: 422400,
+         name: '고양이',
+      },
+   ],
 });
 const stateData = atom({
-  key: "state",
-  default: [
-    {
-      id: "notice",
-      name: "공고중",
-    },
-    {
-      id: "protect",
-      name: "보호중",
-    },
-  ],
+   key: 'state',
+   default: [
+      {
+         id: 'notice',
+         name: '공고중',
+      },
+      {
+         id: 'protect',
+         name: '보호중',
+      },
+   ],
 });
 const yesDataState = atom({
-  key: "Yes",
-  default: [
-    {
-      id: "Y",
-      name: "중성화 O",
-    },
-    {
-      id: "N",
-      name: "중성화 X",
-    },
-    {
-      id: "U",
-      name: "미상",
-    },
-  ],
+   key: 'Yes',
+   default: [
+      {
+         id: 'Y',
+         name: '중성화 O',
+      },
+      {
+         id: 'N',
+         name: '중성화 X',
+      },
+      {
+         id: 'U',
+         name: '미상',
+      },
+   ],
 });
 const petindexDataState = atom({
-  key: "petindex",
-  default: [
-    {
-      sexCd: "",
-      kindCd: "",
-      noticeNo: "",
-      processState: "",
-      careAddr: "",
-      noticeSdt: "",
-      weight: "",
-      chargeNm: "",
-      desertionNo: "",
-      careNm: "",
-      careTel: "",
-      happenPlace: "",
-      officatel: "",
-      orgNm: "",
-      filename: "",
-      popfile: "",
-      noticeEdt: "",
-      netureYn: "",
-      specailMark: "",
-      colorCd: "",
-      happenDt: "",
-      age: "",
-    },
-  ],
+   key: 'petindex',
+   default: [
+      {
+         sexCd: '',
+         kindCd: '',
+         noticeNo: '',
+         processState: '',
+         careAddr: '',
+         noticeSdt: '',
+         weight: '',
+         chargeNm: '',
+         desertionNo: '',
+         careNm: '',
+         careTel: '',
+         happenPlace: '',
+         officatel: '',
+         orgNm: '',
+         filename: '',
+         popfile: '',
+         noticeEdt: '',
+         netureYn: '',
+         specailMark: '',
+         colorCd: '',
+         happenDt: '',
+         age: '',
+      },
+   ],
+});
+const petcardDataState = atom({
+   key: 'petcard',
+   default: [
+      {
+         id: '',
+         sexCd: '',
+         kindCd: '',
+         noticeNo: '',
+         processState: '',
+         careAddr: '',
+         noticeSdt: '',
+         weight: '',
+         chargeNm: '',
+         desertionNo: '',
+         careNm: '',
+         careTel: '',
+         happenPlace: '',
+         officetel: '',
+         orgNm: '',
+         filename: '',
+         popfile: '',
+         noticeEdt: '',
+         netureYn: '',
+         specailMark: '',
+         colorCd: '',
+         happenDt: '',
+         age: '',
+      },
+   ],
 });
 export {
-  gunguDataState,
-  placeDataState,
-  indexDataState,
-  animalDataState,
-  stateData,
-  yesDataState,
-  petindexDataState,
+   gunguDataState,
+   placeDataState,
+   indexDataState,
+   animalDataState,
+   stateData,
+   yesDataState,
+   petindexDataState,
+   petcardDataState,
 };
