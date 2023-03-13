7252e0d50d567c06b044bfc9894d6e831dc6aed2
Dark-jin <chch4lee1@naver.com>
2023-03-13 13:04:33 +0900

    fix: mainpage css

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -21,7 +21,6 @@ const Main = () => {
     navigate("/Latestsearch");
   }
   const loginClick = () => {
-    //navigate("/Loginpage");
     navigate("/Auth");
   };
 
@@ -29,18 +28,20 @@ const Main = () => {
     <>
       <Lawdialog open={open} onClose={handleClose} />
       <div className="h-full w-full flex flex-col">
-        <div className="ml-10">
-          <h1 className="text-green-700">MJ PET</h1>
-          <a href={KAKAO_AUTH_URL}>
-            <Button variant="contained" size="medium" onClick={loginClick}>
-              로그인
-            </Button>
-          </a>
-          <div className="text-right pr-16">
-            <Button variant="contained" size="medium" onClick={handleClick}>
-              최근 조회
-            </Button>
+        <div className="ml-10 mt-5">
+          <div className="text-6xl font-bold">MJ PET</div>
+          <div className="text-right pr-16 mb-4">
+            <a href={KAKAO_AUTH_URL}>
+              <Button variant="contained" size="medium" onClick={loginClick}>
+                로그인
+              </Button>
+            </a>
           </div>
+        </div>
+        <div className="text-right pr-16">
+          <Button variant="contained" size="medium" onClick={handleClick}>
+            최근 조회
+          </Button>
         </div>
         <div className="left-1/2 mt-10">
           <Search />
