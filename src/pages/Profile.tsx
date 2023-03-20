90cd7b1ac6e63596b7e67f8c2a5adbff2bb0e8c0
Dark-jin <chch4lee1@naver.com>
2023-03-20 19:18:12 +0900

    fix: profile page ui

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,6 +4,7 @@ import { ProfileAPI } from "../api/auth";
 import { useRecoilState } from "recoil";
 import { userDataState } from "../states/atom";
 import { useNavigate } from "react-router-dom";
+import { Paper } from "@mui/material";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -20,12 +21,31 @@ const Profile = () => {
       <div className="text-6xl font-bold mt-6 ml-8" onClick={homeClick}>
         MJ PET
       </div>
-      <div className="text-center grid place-items-center">
-        <h1>Profile</h1>
+      <div className="grid grid-cols-2 mt-8">
+        <div className="text-center grid place-items-center">
+          <Paper
+            sx={{
+              height: "600px",
+              width: "400px",
+              paddingTop: 5,
+              paddingBottom: 10,
+              borderRadius: "30px",
+            }}
+            elevation={0}
+            variant="outlined"
+          >
+            <h1>
+              {"<"}Profile{">"}
+            </h1>
+            <div>
+              <img src={user.picture} className="h-full mt-6"></img>
+              <h1>{user.nickname}</h1>
+              <h3>이메일: {user.email}</h3>
+            </div>
+          </Paper>
+        </div>
         <div>
-          <img src={user.picture} className="h-full mt-6"></img>
-          <h1>{user.nickname}</h1>
-          <h3>이메일: {user.email}</h3>
+          <h1>내가 관심 있는 유기동물</h1>
         </div>
       </div>
     </>
