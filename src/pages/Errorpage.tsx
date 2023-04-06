533247c8d38bcc5f5a099329dbed6e66b49d6b82
Dark-jin <chch4lee1@naver.com>
2023-04-06 19:08:06 +0900

    feat: Errorpage animation

--- src/pages/Errorpage.tsx
+++ src/pages/Errorpage.tsx
@@ -1,8 +1,21 @@
+import erroranimation from "../assets/erroranimation.json";
+import Lottie from "react-lottie";
+
 const Errorpage = () => {
   return (
-    <div>
-      <h1>에러페이지</h1>
-    </div>
+    <Lottie
+      options={{
+        loop: true,
+        autoplay: true,
+        animationData: erroranimation,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice",
+        },
+      }}
+      height={400}
+      width={400}
+      isClickToPauseDisabled
+    />
   );
 };
 export default Errorpage;
